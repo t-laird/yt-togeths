d352e0f2dc3e0aae269edf2a2f7ddedfefc4a457
Thomas Laird <t6r6l5@gmail.com>
2019-12-10 21:31:18 -0700

    more fe setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
 import React, { Component, Fragment } from 'react';
 import { Switch, Route } from 'react-router';
 import { withRouter } from 'react-router';
-import './App.css';
+import './App.scss';
 import io from 'socket.io-client';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      formValue: ''
     };
     this.socket = io();
 
     this.socket.on('message', message => {
     });
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({target: {value}}) {
+    this.setState({formValue: value});
   }
 
 
   render() {
     return (
       <div className="App">
+        <div class="form">
+          <input type="text" value={this.state.formValue} onChange={this.handleChange} placeholder="enter youtube URL"/>
+          <button onClick={this.handleSubmit}>Submit</button>
+
+
+        </div>
       </div>
     );
   }
