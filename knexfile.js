9f20d167147dd7751ed820361e1c5b0ab389bd5f
Thomas Laird <t6r6l5@gmail.com>
2019-12-10 20:49:51 -0700

    boilerplate

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/newhome',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/newhome_test', 
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    }
+  }
+};
