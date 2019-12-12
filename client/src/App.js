14aee0ce33ca0abf86b7c512723f57f55335734b
CaseyMichael <caseymichaelpeters@gmail.com>
2019-12-12 00:35:57 -0700

    Get rid of unused stuff

--- client/src/App.js
+++ client/src/App.js
@@ -53,23 +53,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="form">
-          <input type="text" value={this.state.formValue} onChange={this.handleChange} placeholder="enter youtube URL"/>
-          <button onClick={this.handleSubmit}>Submit</button>
-        </div>
         <input type="text" value={this.roomId} onChange={this.updateRoomId}></input>
         <button onClick={this.join}>join</button>
         <button onClick={this.leave}>leave</button>
         <button onClick={this.play}>play</button>
         <button onClick={this.debug}>debug</button>
-        <div class="yt-embedded-player">
-          <embed src="http://www.youtube.com/v/GlIzuTQGgzs?version=3&amp;hl=en_US&amp;rel=0&amp;autohide=1&amp;autoplay=1" 
-            width="100%"
-            height="100%"
-            wmode="transparent" 
-            type="application/x-shockwave-flash" 
-            allowfullscreen="true" title="Adobe Flash Player" />
-        </div>
         <Switch>
           <Route component={Room} path="/watch/:uuid"></Route>
           <Route component={Home} path="/"></Route>
