1c7ddf87ff4e1fa9ed803af3e0424b5819baf939
CaseyMichael <caseymichaelpeters@gmail.com>
2019-12-12 00:22:05 -0700

    created all the server functionality for joining leaving and play

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+"use strict";
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+const winston = require('winston');
 const path = require('path');
-
 const environment = process.env.NODE_ENV || 'development';
 const config = require('./knexfile')[environment];
 const database = require('knex')(config);
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({ format: winston.format.simple() })
+  ]
+});
+
 
 app.use('/', express.static(`${__dirname}/client/build`));
 app.set('port', process.env.PORT || 5000);
@@ -16,23 +25,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-let socketRooms = [];
-
 io.on('connection', socket => {
   io.sockets.emit('welcome', 'hi ⛑️');
 
-  socket.on('connect', () => {
-    console.log('client connected');
+  socket.on('debug', () => {
+    logger.info(`${JSON.stringify(socket.rooms)}`);
   });
 
-  socket.on('join-room', joinrequest => {
-    socket.join(joinrequest.room.id)
-    io.to(joinrequest.room.id).emit('welcome', `User ${socket.id} has joined the room.`)
+  socket.on('join-room', roomId => {
+    socket.join(roomId, () => {
+      logger.info(`${socket.id} has joined room: ${roomId}`);
+    });
   });
 
-  socket.on('leave-room', leaverequest => {
-    socket.leave(leaverequest.room.id)
-    io.to(joinrequest.room.id).emit('GoodBye', `User ${socket.id} has left the room.`)
+  socket.on('leave-room', roomId => {
+    socket.leave(roomId, () => {
+      logger.info(`${socket.id} has left room: ${roomId}`);
+    });
+  });
+
+  socket.on('play', roomId => {
+    io.to(roomId).emit('play');
+      logger.info(`${socket.id} pressed play to room: ${roomId}`);
   });
 
 });
