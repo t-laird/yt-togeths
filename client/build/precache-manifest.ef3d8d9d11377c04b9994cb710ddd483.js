3ad3a503e0f1de3ba92a256bcf87dcb3b18b2cfa
Thomas Laird <t6r6l5@gmail.com>
2019-12-14 18:10:40 -0700

    commit build files

---
+++ client/build/precache-manifest.ef3d8d9d11377c04b9994cb710ddd483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b843c39b218a18475bdb5dde6eff0e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d614e78b4c044f0759",
+    "url": "/static/css/main.e8379c7a.chunk.css"
+  },
+  {
+    "revision": "4a3a984609d0988214d1",
+    "url": "/static/js/2.17f89cf9.chunk.js"
+  },
+  {
+    "revision": "727c1d8a8c6d5ba475921c6a64315e87",
+    "url": "/static/js/2.17f89cf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16d614e78b4c044f0759",
+    "url": "/static/js/main.8366ddd0.chunk.js"
+  },
+  {
+    "revision": "7027778f18f2a8f92df8",
+    "url": "/static/js/runtime-main.adcfc9bd.js"
+  },
+  {
+    "revision": "93e18e1073faa16d58f3812c333f3d43",
+    "url": "/static/media/bg.93e18e10.svg"
+  }
+]);
